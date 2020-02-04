1e48bcd31e6287ddc1d5ff4fe3bdb7c62d278769
17862955008@163.com <songxujing520>
2020-02-04 16:55:06 +0800

    test

--- src/com/cmb/HelloWorld.java
+++ src/com/cmb/HelloWorld.java
@@ -2,7 +2,16 @@ package com.cmb;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = sum(a , b);
         System.out.println("hello world!");
+    }
+
+
+
+    public static int sum(int a, int b) {
+        return a+b;
     }
 
 }
